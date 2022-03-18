66fd78a335f6fc55cd3acbeb7e371ffe329bc495
JimmyJ0ee <98901860+JimmyJ0ee@users.noreply.github.com>
2022-03-18 20:08:27 +0100

    Add files via upload

--- ausgabe.c
+++ ausgabe.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 //Eingabe Gruppeneinteilung, E-Mailadressen, Anzahl an Gruppen -> beste Format wäre Liste mit Listennamen = Gruppennamen + Listeninhalt = E-Mailadressen
 //Ausgabe Gruppenzuordnung formatiert in der Konsole 
 //gruppeneinteilung[*(gr_ausg-1)]
-int ausgabe(char **gruppeneinteilung, int anz_gr_1, int mitgl_1){
+
+int main(){
+    //char*[][]=gruppeneinteilung caro
+    char gruppe_1 [4][8] = {"nico", "steven", "caro", "chris"};
+    char gruppe_2 [4][8] = {"anna", "mogens", "hulia", "tom"};
+    char gruppe_3 [4][8] = {"conni", "josi", "sandra", "eugen"};
+    //char *gruppe_4 [] = {"gustav", "bernd"};
+    int anz_gr_1=3;
+    int mitgl_1=4;
+    char groups[anz_gr_1][mitgl_1][8];
+    memcpy(groups[0], gruppe_1, sizeof(groups[0]));
+    memcpy(groups[1], gruppe_2, sizeof(groups[1]));
+    memcpy(groups[2], gruppe_3, sizeof(groups[2])); 
     int gr_ausg = anz_gr_1-(anz_gr_1-1);
     int mitgl_zaheler = mitgl_1-(mitgl_1-1);
     for (anz_gr_1;anz_gr_1!=0;anz_gr_1--){ 
@@ -11,23 +24,12 @@ int ausgabe(char **gruppeneinteilung, int anz_gr_1, int mitgl_1){
         gr_ausg++;
     }
         for (mitgl_1;mitgl_1!=0;mitgl_1--){
-            printf("%s ", gruppeneinteilung[gr_ausg][mitgl_zaheler]);
+            printf("%s ", groups[gr_ausg][mitgl_zaheler]);
             mitgl_zaheler++;
 
 
     }
-}
-
-int main(){
-    //char*[][]=gruppeneinteilung caro
-    char *gruppe_1 [] = {"nico", "steven", "caro", "chris"};
-    char *gruppe_2 [] = {"anna", "mogens", "hulia", "tom"};
-    char *gruppe_3 [] = {"conni", "josi", "sandra", "eugen"};
-    //char *gruppe_4 [] = {"gustav", "bernd"};
-    int anz_gr_1=4;
-    int mitgl_1=4;
-    char *gruppeneinteilung [] ={gruppe_1, gruppe_2, gruppe_3}; 
    // char* gruppeneinteilung[14] = {"nico", "steven", "caro", "chris", "anna", "mogens", "hulia", "tom", "conni", "josi", "sandra", "eugen", "gustav", "bernd"};
-    ausgabe(gruppeneinteilung, anz_gr_1, mitgl_1);
+   // ausgabe(groups, anz_gr_1, mitgl_1);
 
 }

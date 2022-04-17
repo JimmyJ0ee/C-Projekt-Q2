4b5f5cde54a26985883bf2825a7e02af886f55f5
JimmyJ0ee <98901860+JimmyJ0ee@users.noreply.github.com>
2022-04-17 17:17:15 +0200

    Add files via upload
    Denkt daran den Ordnerpfad zu ändern.

--- file_test_win.c
+++ file_test_win.c
@@ -1,12 +1,59 @@
+// To-Do
+//      - pos_case datei muss 1x am Tag gecleared werden! oder einen neue Liste erstellt werden! -> done
+//      - alle Dateien müssen durchiteriert werden (brauche: Verzeichnisinhalt + Dateinamen + Anzahl von Dateien in dem Verzeichnis)
+//      - ctrl + c, um Datei zu beenden
 #include <windows.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <assert.h>
+#include <time.h>
+
+// Liste mit den positiven Fällen wird erstellt -> Gesundheitsamt
+void add_file_to_pos_case_list(char* dateiname){
+    FILE* testdatei;
+    FILE* pos_cases;
+    char inhalt[80];
+    testdatei = fopen(dateiname,"r");
+    pos_cases = fopen("pos_case.txt","a"); 
+    for(uint8_t i=0;i<3;i++){
+        fscanf(testdatei, "%s",inhalt);
+        fprintf(pos_cases,"%s\t", inhalt);
+   }
+   fprintf(pos_cases,"\n");
+   fclose(testdatei);
+   fclose(pos_cases);
+}
+void pos_case_reset(){
+    fopen("pos_case.txt","w");
+}
+// Ergebnis des Tests wird in die Testanfrage geschrieben!
+void write_result_in_file(char* result, char* dateiname){
+    FILE* test_write_result;
+    test_write_result = fopen(dateiname,"a");
+    fprintf(test_write_result,"\n%s", result);
+    fclose(test_write_result);
+}
+// Auswerten einer Datei nach Eingabe von "r"
+void after_exit(char* dateiname){
+    srand(time(NULL));
+    //uint8_t a= rand()%2;
+    int a=0;
+    char* result;
+    if (a==0){  //positiver Test
+        result="positiv";
+        write_result_in_file(result, dateiname);
+        add_file_to_pos_case_list(dateiname);
+    }
+    else{       //negativer Test
+        result="negativ";
+        write_result_in_file(result, dateiname);
+    }
+}
 
 int main() {
-    char *path = "C:\\Users\\auchterc\\C\\abgabe_2"; // Pfad zum ueberwachten Verzeichnis.
-
+    char *path = "C:/Users/Z0127829/OneDrive - ZF Friedrichshafen AG/Desktop/DHBW/Programmieren/C und C++/Abgabe_2"; // Pfad zum ueberwachten Verzeichnis.
+    pos_case_reset();
     // Handle fuer das Verzeichnis
     HANDLE file = CreateFile(path,
                              FILE_LIST_DIRECTORY,
@@ -33,37 +80,36 @@ int main() {
                        NULL, &overlapped, NULL);
 
     // es geht los
-    printf("Verzeichnis %s wird auf Aendeurngen ueberprueft...\n", path);
+    printf("warten %s auf Änderungen...\n", path);
 
     while (true) {
         DWORD result = WaitForSingleObject(overlapped.hEvent, 0);
-
+        char probe = getchar();             // stört Funktion des Programms
         if (result == WAIT_OBJECT_0) {
             DWORD bytes_transferred;
             GetOverlappedResult(file, &overlapped, &bytes_transferred, FALSE);
-
             FILE_NOTIFY_INFORMATION *event = (FILE_NOTIFY_INFORMATION*)change_buf;
             while(true) {
                 DWORD name_len = event->FileNameLength / sizeof(wchar_t);
 
                 switch (event->Action) {
                 case FILE_ACTION_ADDED: {
-                    wprintf(L"      Datei hinzugefuegt: %.*s\n", name_len, event->FileName);
+                    wprintf(L"       Hinzugefügt: %.*s\n", name_len, event->FileName);
                 }
                 break;
 
                 case FILE_ACTION_REMOVED: {
-                    wprintf(L"      Datei geloescht: %.*s\n", name_len, event->FileName);
+                    wprintf(L"     Gelöscht: %.*s\n", name_len, event->FileName);
                 }
                 break;
 
                 case FILE_ACTION_MODIFIED: {
-                    wprintf(L"      Datei veraendert: %.*s\n", name_len, event->FileName);
+                    wprintf(L"    Verändert: %.*s\n", name_len, event->FileName);
                 }
                 break;
 
                 case FILE_ACTION_RENAMED_OLD_NAME: {
-                    wprintf(L"      Datei umbenannt von: %.*s\n", name_len, event->FileName);
+                    wprintf(L"Name geändert von: %.*s\n", name_len, event->FileName);
                 }
                 break;
 
@@ -73,7 +119,7 @@ int main() {
                 break;
 
                 default: {
-                    printf("Ungueltige Eingabe!\n");
+                    printf("Unerwartete Funktion\n");
                 }
                 break;
                 }
@@ -95,9 +141,17 @@ int main() {
                                NULL, &overlapped, NULL);
 
         }
-
+        //ctrl + c workaround, um Programm zu beenden
+        if(probe == 'r')
+        {
+            //printf("%s", DWORD);
+            char* dateiname = "mus_test.txt";
+            printf("\nklappt!");
+            after_exit(dateiname);
+            break;  
+        }
+        
         // Do other loop stuff here...
     }
 
-    //printf("Hier Ctrl + C abfangen?");
 }
